aab7449f92e1ebf3e02eeb93a56341fb8e27695e
Pandu Rendradjaja <prendradjaja@gmail.com>
2018-04-16 20:23:29 -0700

    ++

--- tick.py
+++ tick.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-import json
+import datetime
 
-def get_event():
-    path = './example-event.json'
-    with open(path) as f:
-        return json.load(f)
+import entry
+import constants
+import timeutil
 
-print(get_event())
+
+def start(description='', color=Color.BLUE):
+    date = datetime.date.today().isoformat()
+    start = timeutil.now()
+    stop = None
+    tags = get_tags(description, color)
+
+    e = create(date, start, stop, color, description, tags)
+    persist(e)
+
+
+def get_tags(description, color):
+    tags = [word[1:] for word in description.split()
+            if word.startswith('#')]
+    return tags + [color]
